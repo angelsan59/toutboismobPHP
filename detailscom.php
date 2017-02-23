4de3c7f2054c1e505c9f6d359ec935e76de1b1a5
sociepka <sociepka@59013STA6101307.afpa-roubaix.local>
2017-02-23 13:31:11 +0100

    1ere version finale avec login et liste séparés

--- detailscom.php
+++ detailscom.php
@@ -1,23 +1,36 @@
 <?php
+
 session_start(); // On démarre la session AVANT toute chose
 require_once("conn.php");
-$idCommande=$_GET['idCommande'];
-$ps=$pdo->prepare("SELECT * FROM ligne_commande JOIN produit ON ligne_commande.code_prod=produit.code_prod WHERE id_com=?");
+$idCommande = $_GET['idCommande'];
+//$ps = $pdo->prepare("SELECT * FROM ligne_commande lc JOIN produit ON ligne_commande.code_prod=produit.code_prod WHERE id_com=?");
+$ps = $pdo->prepare("SELECT c.date_com , s.libelle_statut,p.designation,p.image,p.pu,lc.qte " .
+        "FROM ligne_commande lc,commande c, statut s, produit p  " .
+        "where lc.code_prod=p.code_prod and s.code_statut=c.code_statut and c.id_com = lc.id_com " .
+        "and  c.id_com=?");
 $ps->execute(array($idCommande));
-$liste=$ps->fetchAll(PDO::FETCH_ASSOC);
-/*header("Access-Control-Allow-Origin: *");
-header("Content-Type:application/json; charset=utf-8");
-echo(json_encode($liste)); */
+$liste = $ps->fetchAll(PDO::FETCH_ASSOC);
+/* header("Access-Control-Allow-Origin: *");
+  header("Content-Type:application/json; charset=utf-8");
+  echo(json_encode($liste)); */
 
-$data=array();
-foreach($liste as $i=>$v){
-    $fields=array();
-    foreach($v as $key=>$value){
+$data = array();
+$montant_ht = 0;
+foreach ($liste as $i => $v) {
+    $fields = array();
+    foreach ($v as $key => $value) {
         $fields[$key] = utf8_encode($value);
     }
-    $data[$i]=$fields;
+    $montant_ht = $montant_ht + $fields['pu'] * $fields['qte'];
+    $montant_ttc = $montant_ht * 1.2;
+    $montant_tva = $montant_ht * 0.2;
+
+    $fields['montant_ht'] = utf8_encode($montant_ht);
+    $fields['montant_ttc'] = utf8_encode($montant_ttc);
+    $fields['montant_tva'] = utf8_encode($montant_tva);
+
+    $data[$i] = $fields;
 }
 header("Access-Control-Allow-Origin: *");
-    header("Content-Type:application/json; charset=utf-8");
-    echo(json_encode($data));
-   
+header("Content-Type:application/json; charset=utf-8");
+echo(json_encode($data));
--- listecommandes.php
+++ listecommandes.php
@@ -1,23 +1,32 @@
 <?php
+
 session_start(); // On démarre la session AVANT toute chose
 require_once("./conn.php");
-$ps=$pdo->prepare("SELECT * FROM commande ORDER BY id_com DESC");
-$ps->execute();
-$liste=$ps->fetchAll(PDO::FETCH_ASSOC);
-/*header("Access-Control-Allow-Origin: *");
-header("Content-Type:application/json; charset=utf-8");
-echo(json_encode($liste)); */
+$data = array();
+if (!isset($_SESSION['id_user'])) {
+    $req = "SELECT id_com,date_com,date_livr, enseigne, libelle_statut FROM commande,fournisseur,statut "
+            . "where commande.code_fou=fournisseur.code_fou and commande.code_statut=statut.code_statut "
+            . "ORDER BY id_com ASC";
+    $ps = $pdo->prepare($req);
+    $ps->execute();
+    $liste = $ps->fetchAll(PDO::FETCH_ASSOC);
 
-$data=array();
-foreach($liste as $i=>$v){
-    $fields=array();
-    foreach($v as $key=>$value){
-        $fields[$key] = utf8_encode($value);
+    foreach ($liste as $i => $v) {
+        $fields = array();
+        foreach ($v as $key => $value) {
+            $fields[$key] = utf8_encode($value);
+        }
+        $data[$i] = $fields;
     }
-    $data[$i]=$fields;
+} else {
+    $fields = array();
+    $value = 'Veuillez vous connecter pour voir les commandes';
+    $fields['erreur'] = utf8_encode($value);
+    $fields['id_com'] = utf8_encode('0');
+    $data[0] = $fields;
 }
+
 header("Access-Control-Allow-Origin: *");
-    header("Content-Type:application/json; charset=utf-8");
-    echo(json_encode($data));
-   
-    ?>
+header("Content-Type:application/json; charset=utf-8");
+echo(json_encode($data));
+?>
